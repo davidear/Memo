06bed3832542ba71fbbb3ca138b3a897ffb6c1a8
dai.fengyi <fengji@zkjinshi.com>
2015-07-30 21:18:15 +0800

    MMNoteEditViewController

--- Memo/Module/Note/Controller/MMNoteCollectionViewController.swift
+++ Memo/Module/Note/Controller/MMNoteCollectionViewController.swift
@@ -11,7 +11,7 @@ var i = 0
 class MMNoteLayout: UICollectionViewFlowLayout {
   override func shouldInvalidateLayoutForBoundsChange(newBounds: CGRect) -> Bool {
     i++
-//    println("shouldInvalidateLayoutForBoundsChange \(i)")
+    //    println("shouldInvalidateLayoutForBoundsChange \(i)")
     return true
   }
   
@@ -20,23 +20,15 @@ class MMNoteLayout: UICollectionViewFlowLayout {
     let offset = self.collectionView!.contentOffset
     var attrsArray = NSMutableArray(array: super.layoutAttributesForElementsInRect(rect)!)
     let topMinY = -collectionView!.contentInset.top
-   let bottomMaxY = max(collectionView!.contentSize.height + collectionView!.contentInset.bottom - UIScreen.mainScreen().bounds.height, 0)
+    let bottomMaxY = max(collectionView!.contentSize.height + collectionView!.contentInset.bottom - UIScreen.mainScreen().bounds.height, 0)
     
     if offset.y < topMinY {
       let headerSize = self.headerReferenceSize
-      let deltaY = fabsf(Float(offset.y - topMinY))
+      let deltaY = fabsf(Float(offset.y - topMinY) / 10)
       
       for a in attrsArray {
         if let attrs = a as? UICollectionViewLayoutAttributes {//从NSMutableArray中取元素，必须unwrap
-          if attrs.representedElementKind != nil {//必须判断是否有值
-            //            if attrs.representedElementKind == UICollectionElementKindSectionHeader {//nil是不能和String进行相等运算的
-            //              var headerRect = attrs.frame
-            //              headerRect.size.height = headerSize.height + CGFloat(deltaY)
-            //              println("topMinY is \(topMinY), height is \(headerRect.size.height)")
-            //              headerRect.origin.y  = headerRect.origin.y - CGFloat(deltaY) + CGFloat(deltaY) * CGFloat(attrs.indexPath.section)
-            //              attrs.frame = headerRect
-            //            }
-          }else {
+          if attrs.representedElementKind == nil {//必须判断是否有值
             var headerRect = attrs.frame
             headerRect.origin.y = CGFloat(Float(headerRect.origin.y) + deltaY * Float(attrs.indexPath.section))
             attrs.frame = headerRect
@@ -44,25 +36,18 @@ class MMNoteLayout: UICollectionViewFlowLayout {
         }
       }
     }
+    
     println("offset is \(offset.y), collectionView contentSize is \(collectionView?.contentSize.height)")
     if offset.y > bottomMaxY {
       let headerSize = self.headerReferenceSize
-      let deltaY = fabsf(Float(offset.y - bottomMaxY))
+      let deltaY = fabsf(Float(offset.y - bottomMaxY) / 10)
       
       for a in attrsArray {
         if let attrs = a as? UICollectionViewLayoutAttributes {//从NSMutableArray中取元素，必须unwrap
-          if attrs.representedElementKind != nil {//必须判断是否有值
-            //            if attrs.representedElementKind == UICollectionElementKindSectionHeader {//nil是不能和String进行相等运算的
-            //              var headerRect = attrs.frame
-            //              headerRect.size.height = headerSize.height + CGFloat(deltaY)
-            //              println("topMinY is \(topMinY), height is \(headerRect.size.height)")
-            //              headerRect.origin.y  = headerRect.origin.y - CGFloat(deltaY) + CGFloat(deltaY) * CGFloat(attrs.indexPath.section)
-            //              attrs.frame = headerRect
-            //            }
-          }else {
+          if attrs.representedElementKind == nil {//必须判断是否有值
             var headerRect = attrs.frame
-//            headerRect.origin.y = CGFloat(Float(headerRect.origin.y) + deltaY * Float(attrs.indexPath.section))
-            headerRect.origin.y = collectionView!.contentSize.height - 10 - CGFloat(deltaY) * CGFloat(collectionView!.numberOfSections() - attrs.indexPath.section) - CGFloat(20) * CGFloat(collectionView!.numberOfSections() - attrs.indexPath.section - 1) - CGFloat(44) * CGFloat(collectionView!.numberOfSections() - attrs.indexPath.section)
+            //            headerRect.origin.y = CGFloat(Float(headerRect.origin.y) + deltaY * Float(attrs.indexPath.section))
+            headerRect.origin.y = collectionView!.contentSize.height + collectionView!.contentInset.bottom - 10 - CGFloat(deltaY) * CGFloat(collectionView!.numberOfSections() - attrs.indexPath.section) - CGFloat(20) * CGFloat(collectionView!.numberOfSections() - attrs.indexPath.section - 1) - CGFloat(44) * CGFloat(collectionView!.numberOfSections() - attrs.indexPath.section)
             attrs.frame = headerRect
           }
         }
@@ -71,108 +56,113 @@ class MMNoteLayout: UICollectionViewFlowLayout {
     return attrsArray as [AnyObject]
   }
 }
-let reuseIdentifier = "Cell"
 
-class MMNoteCollectionViewCell: UICollectionViewCell {
-  @IBOutlet weak var logo: UIImageView!
-  @IBOutlet weak var name: UILabel!
-  @IBOutlet weak var ammount: UILabel!
-}
-
-class MMNoteCollectionViewController: UICollectionViewController {
-  var dataArray: Array<Dictionary<String,String>>!
-  @IBOutlet weak var noteLayout: MMNoteLayout!
+  let reuseIdentifier = "Cell"
   
-  required init(coder aDecoder: NSCoder) {
-    super.init(coder: aDecoder)
-    loadData()
-  }
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    setUI()
+  class MMNoteCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var ammount: UILabel!
   }
   
-  private func loadData() {
-    let path = NSBundle.mainBundle().pathForResource("NoteList", ofType: "plist")
-    let arr = NSArray(contentsOfFile: path!)
-    dataArray = arr as! Array
-  }
-  
-  /*    
-  var minimumLineSpacing: CGFloat
-  var minimumInteritemSpacing: CGFloat
-  var itemSize: CGSize
-  @availability(iOS, introduced=8.0)
-  var estimatedItemSize: CGSize // defaults to CGSizeZero - setting a non-zero size enables cells that self-size via -preferredLayoutAttributesFittingAttributes:
-  var scrollDirection: UICollectionViewScrollDirection // default is UICollectionViewScrollDirectionVertical
-  var headerReferenceSize: CGSize
-  var footerReferenceSize: CGSize
-  var sectionInset: UIEdgeInsets*/
-  private func setUI() {
-    noteLayout.itemSize = CGSizeMake(UIScreen.mainScreen().bounds.width - 40, 44)
-//    noteLayout.headerReferenceSize = CGSizeMake(UIScreen.mainScreen().bounds.width - 40, <#height: CGFloat#>)
-    noteLayout.sectionInset = UIEdgeInsetsMake(10, 10, 10, 10)
-  }
-  /*
-  // MARK: - Navigation
-  
-  // In a storyboard-based application, you will often want to do a little preparation before navigation
-  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-  // Get the new view controller using [segue destinationViewController].
-  // Pass the selected object to the new view controller.
-  }
-  */
-  
-  // MARK: UICollectionViewDataSource
-  
-  override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
-    //#warning Incomplete method implementation -- Return the number of sections
-    return dataArray.count
-  }
-  
-  
-  override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    //#warning Incomplete method implementation -- Return the number of items in the section
-    return 1
-  }
-  
-  override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-    let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! UICollectionViewCell
-
-    // Configure the cell
+  class MMNoteCollectionViewController: UICollectionViewController {
+    var dataArray: Array<Dictionary<String,String>>!
+    @IBOutlet weak var noteLayout: MMNoteLayout!
     
-    return cell
-  }
-  
-  // MARK: UICollectionViewDelegate
-  
-  /*
-  // Uncomment this method to specify if the specified item should be highlighted during tracking
-  override func collectionView(collectionView: UICollectionView, shouldHighlightItemAtIndexPath indexPath: NSIndexPath) -> Bool {
-  return true
-  }
-  */
-  
-  /*
-  // Uncomment this method to specify if the specified item should be selected
-  override func collectionView(collectionView: UICollectionView, shouldSelectItemAtIndexPath indexPath: NSIndexPath) -> Bool {
-  return true
-  }
-  */
-  
-  /*
-  // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-  override func collectionView(collectionView: UICollectionView, shouldShowMenuForItemAtIndexPath indexPath: NSIndexPath) -> Bool {
-  return false
-  }
-  
-  override func collectionView(collectionView: UICollectionView, canPerformAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) -> Bool {
-  return false
-  }
-  
-  override func collectionView(collectionView: UICollectionView, performAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) {
-  
-  }
-  */
-  
+    required init(coder aDecoder: NSCoder) {
+      super.init(coder: aDecoder)
+      loadData()
+    }
+    override func viewDidLoad() {
+      super.viewDidLoad()
+      setUI()
+    }
+    
+    private func loadData() {
+      let path = NSBundle.mainBundle().pathForResource("NoteList", ofType: "plist")
+      let arr = NSArray(contentsOfFile: path!)
+      dataArray = arr as! Array
+    }
+    
+    /*
+    var minimumLineSpacing: CGFloat
+    var minimumInteritemSpacing: CGFloat
+    var itemSize: CGSize
+    @availability(iOS, introduced=8.0)
+    var estimatedItemSize: CGSize // defaults to CGSizeZero - setting a non-zero size enables cells that self-size via -preferredLayoutAttributesFittingAttributes:
+    var scrollDirection: UICollectionViewScrollDirection // default is UICollectionViewScrollDirectionVertical
+    var headerReferenceSize: CGSize
+    var footerReferenceSize: CGSize
+    var sectionInset: UIEdgeInsets*/
+    private func setUI() {
+      noteLayout.itemSize = CGSizeMake(UIScreen.mainScreen().bounds.width - 40, 44)
+      //    noteLayout.headerReferenceSize = CGSizeMake(UIScreen.mainScreen().bounds.width - 40, <#height: CGFloat#>)
+      noteLayout.sectionInset = UIEdgeInsetsMake(10, 10, 10, 10)
+    }
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+    }
+    */
+    
+    // MARK: UICollectionViewDataSource
+    
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+      //#warning Incomplete method implementation -- Return the number of sections
+      return dataArray.count
+    }
+    
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+      //#warning Incomplete method implementation -- Return the number of items in the section
+      return 1
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+      let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! UICollectionViewCell
+      
+      // Configure the cell
+      
+      return cell
+    }
+    
+    // MARK: UICollectionViewDelegate
+//    override func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+//      let noteEditVC = self.storyboard!.instantiateViewControllerWithIdentifier("NoteEditViewController") as! MMNoteEditViewController
+//      self.navigationController!.pushViewController(noteEditVC, animated: true)
+//    }
+    
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(collectionView: UICollectionView, shouldHighlightItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+    return true
+    }
+    */
+    
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(collectionView: UICollectionView, shouldSelectItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+    return true
+    }
+    */
+    
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(collectionView: UICollectionView, shouldShowMenuForItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+    return false
+    }
+    
+    override func collectionView(collectionView: UICollectionView, canPerformAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) -> Bool {
+    return false
+    }
+    
+    override func collectionView(collectionView: UICollectionView, performAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) {
+    
+    }
+    */
+    
 }
---
+++ Memo/Module/Note/Controller/MMNoteEditViewController.swift
@@ -0,0 +1,42 @@
+//
+//  MMNoteEditViewController.swift
+//  Memo
+//
+//  Created by dai.fengyi on 15/7/30.
+//  Copyright (c) 2015年 childrenOurFuture. All rights reserved.
+//
+
+import UIKit
+
+class MMNoteEditViewController: UIViewController {
+  @IBOutlet weak var location: UILabel!
+  @IBOutlet weak var notebook: UIButton!
+  @IBOutlet weak var map: UIButton!
+  @IBOutlet weak var editTextView: UITextView!
+  @IBOutlet weak var scrollView: UIScrollView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+      self.scrollView.frame = self.view.bounds
+      self.scrollView.contentSize = self.scrollView.bounds.size
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
